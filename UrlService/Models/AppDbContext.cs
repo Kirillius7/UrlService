12e602ca1d9aa7fff4291b85583f87bbf2caf034
Kyrylo Popov <popovkirill070320@gmail.com>
2025-10-04 10:21:34 +0300

    Add project files.

---
+++ UrlService/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace UrlService.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ShortUrl> shortUrl { get; set; } // оголошення таблиці (колекції сутностей)
+        // для роботи із даними як із колекцією (а не таблицею і рядком SQL)
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        // підключення до відповідної БД, врахування додаткового функціоналу (lazy loading, логування)
+        // як поводитись із транзакціями та міграціями
+    }
+}
---
+++ UrlService/Models/ShortUrl.cs
@@ -0,0 +1,11 @@
+﻿namespace UrlService.Models
+{
+    public class ShortUrl
+    {
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; }
+        public string ShortCode { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
